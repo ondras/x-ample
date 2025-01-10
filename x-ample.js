dd388695adeb51a9b613799d868fec2011567715
Ondřej Žára <ondrej.zara@gmail.com>
2025-01-10 16:24:13 +0100

    refactor

--- x-ample.js
+++ x-ample.js
@@ -3,6 +3,7 @@ const AsyncGeneratorFunction = (async function*() {}).constructor;
 let connectedXamples = new Set();
 
 export default class ExAmple extends HTMLElement {
+	#mode = "run";
 	#code = "";
 	#generator;
 	#done = false;
@@ -23,16 +24,44 @@ export default class ExAmple extends HTMLElement {
 		this.attachShadow({mode:"open"});
 	}
 
+	get mode() { return this.#mode; }
+	set mode(mode) {
+		this.#mode = mode;
+		let pre = this.#pre;
+		let textarea = this.#textarea;
+		let code = this.#code;
+
+		switch (mode) {
+			case "edit":
+				textarea.style.height = `${pre.offsetHeight}px`;
+				pre.hidden = true;
+				textarea.hidden = false;
+				textarea.value = code;
+			break;
+
+			case "run":
+				textarea.hidden = true;
+				pre.hidden = false;
+				pre.textContent = code;
+				code && this.#run();
+			break;
+		}
+	}
+
 	get code() { return this.#code; }
 	set code(code) {
 		this.#code = code;
-		let func = new AsyncGeneratorFunction(this.#code);
-		this.#generator = func();
-		this.#done = false;
+		if (!this.isConnected) { return; }
 
-		if (this.isConnected) {
-			this.#pre.textContent = this.#code;
-			this.#output.replaceChildren();
+		switch (this.#mode) {
+			case "edit":
+				this.#textarea.value = code;
+			break;
+
+			case "run":
+				this.#pre.textContent = code;
+				code && this.#run();
+			break;
 		}
 	}
 
@@ -42,17 +71,16 @@ export default class ExAmple extends HTMLElement {
 
 		this.#pre.textContent = this.#code;
 		this.#pre.addEventListener("click", _ => {
-			let height = this.#pre.offsetHeight;
-			this.#showTextarea();
-			this.#textarea.style.height = `${height}px`;
+			this.mode = "edit";
 			this.#textarea.focus();
 		});
 
-		this.#hideTextarea();
 		this.#textarea.addEventListener("blur", _ => {
 			this.code = this.#textarea.value;
-			this.#hideTextarea();
+			this.mode = "run";
 		});
+
+		this.mode = "run";
 
 		connectedXamples.add(this);
 	}
@@ -64,23 +92,35 @@ export default class ExAmple extends HTMLElement {
 	async tick() {
 		if (this.#pulling || this.#done) { return; }
 		this.#pulling = true;
-		let { value, done } = await this.#generator.next();
-		this.#pulling = false;
-		this.#done = done;
 
-		if (!done) { this.#output.replaceChildren(value); }
+		let gen = this.#generator;
+		gen.next().then(({value, done}) => {
+			if (gen != this.#generator) { return; }
+			this.#pulling = false;
+			this.#done = done;
+			if (!done) { this.#output.replaceChildren(value); }
+		});
 	}
 
-	#showTextarea() {
-		this.#pre.hidden = true;
-		this.#textarea.hidden = false;
-		this.#textarea.value = this.#code;
-	}
+	#run() {
+		this.#output.replaceChildren();
 
-	#hideTextarea() {
-		this.#textarea.hidden = true;
-		this.#pre.hidden = false;
-		this.#pre.textContent = this.#code;
+		try {
+			if (this.#generator) {
+				this.#generator.return();
+				this.#pulling = false;
+			}
+
+			let func = new AsyncGeneratorFunction(this.#code);
+			this.#generator = func();
+			this.#done = false;
+			this.tick();
+		} catch (e) {
+			this.#generator = undefined;
+			this.#done = true;
+			this.#output.append(e.message);
+		}
+
 	}
 }
 
